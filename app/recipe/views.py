c3f84e5be6bcee2edbae5eb21464817c8ba0e3e4
koresachin7 <koresachin7@gmail.com>
2024-04-05 12:17:42 +0530

    Write Tests And Implement ingredient list API And also ADD update API & Tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -50,4 +51,18 @@ class TagViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """Mange Ingredients in the datababe."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter querset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
