dd77b3098ec968d2f5e70f5262e2c4ae10a973bb
koresachin7 <koresachin7@gmail.com>
2024-04-06 11:43:55 +0530

    Refactor recipe views.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -64,7 +64,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
-        ingredients =validated_data.pop('ingredients', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
 
         self._get_or_create_tags(tags, recipe)
